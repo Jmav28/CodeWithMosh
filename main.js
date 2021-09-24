20492ba7ca1c4a1bc643ed74c4efe3a0a3c7a763
Miguel Avila <miguelavilaa28@gmail.com>
2021-09-24 16:23:17 -0700

    navbar responsive

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const btnNav = document.getElementById('navbar-toggle'),
-        navMenu = document.getElementById('navbar-links');
+        navMenu = document.getElementById('navbar-links'),
+        moreBtn =document.getElementById('more-btn'),
+        moreSect = document.getElementById('more-section');
 
 btnNav.addEventListener('click', () => {
     if(navMenu.classList.contains('activo')) {
@@ -7,5 +9,14 @@ btnNav.addEventListener('click', () => {
     }
     else {
         navMenu.classList.add('activo');
+    }
+});
+
+moreBtn.addEventListener('click', () => {
+    if(moreSect.classList.contains('activo')) {
+        moreSect.classList.remove('activo');
+    }
+    else {
+        moreSect.classList.add('activo');
     }
 });
